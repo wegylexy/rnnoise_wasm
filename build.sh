2bf79f49954eab6ff97c36a385000b914d0faf8d
TimTim Wong <t2@live.hk>
2020-06-28 05:41:13 +0800

    Include default RNN model

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ emcc \
 -O3 -g0 \
 -o dist/rnnoise-processor.wasm \
 -Irnnoise/include \
+rnnoise/src/celt_lpc.c \
 rnnoise/src/denoise.c \
-rnnoise/src/rnn.c \
 rnnoise/src/kiss_fft.c \
 rnnoise/src/pitch.c \
-rnnoise/src/celt_lpc.c \
+rnnoise/src/rnn.c \
+rnnoise/src/rnn_data.c \
 src/worklet.c
 cp src/processor.js dist/rnnoise-processor.js
 cp src/runtime.js dist/rnnoise-runtime.js
