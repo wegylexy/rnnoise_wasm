e2c5a80ca7552c79b1bd1ba167487df236ed6d2c
TimTim Wong <t2@live.hk>
2020-06-29 07:50:06 +0800

    Extract constants

--- src/worklet.c
+++ src/worklet.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <rnnoise.h>
 
-static const float scale = 32768;
-static float inputBuffer[32768], outputBuffer[32768], vad_prob = 0;
+#define FRAME_SIZE 480
+#define MAX_FRAME_SIZE 16384
+
+static const float scale = -INT16_MIN;
+static float inputBuffer[MAX_FRAME_SIZE * 2], outputBuffer[MAX_FRAME_SIZE * 2], vad_prob = 0;
 static size_t input = 0, processed = 0, buffered = 0, output = 0, buffering = 0, latency;
 static DenoiseState *state = NULL;
 
@@ -15,9 +18,9 @@ float *EMSCRIPTEN_KEEPALIVE pipe(size_t length)
 {
     // Increases latency
     if (length > buffering)
-        latency = buffering = (480 / length + (480 % length ? 1 : 0)) * length;
+        latency = buffering = (FRAME_SIZE / length + (FRAME_SIZE % length ? 1 : 0)) * length;
     // Shifts output
-    if (output && buffered > 16384)
+    if (output && buffered > MAX_FRAME_SIZE)
     {
         size_t d = buffered - output;
         for (size_t i = 0; i < d; ++i)
@@ -29,14 +32,14 @@ float *EMSCRIPTEN_KEEPALIVE pipe(size_t length)
     for (size_t end = input + length; input < end; ++input)
         inputBuffer[input] *= scale;
     // Buffers input
-    while (processed + 480 <= input)
+    while (processed + FRAME_SIZE <= input)
     {
         vad_prob = rnnoise_process_frame(state, &outputBuffer[buffered], &inputBuffer[processed]);
-        processed += 480;
-        buffered += 480;
+        processed += FRAME_SIZE;
+        buffered += FRAME_SIZE;
     }
     // Shifts input
-    if (processed && input > 16384)
+    if (processed && input > MAX_FRAME_SIZE)
     {
         size_t d = input - processed;
         for (size_t i = 0; i < d; ++i)
