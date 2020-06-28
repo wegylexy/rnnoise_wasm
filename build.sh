cbec508aa4598bd197ece6f39c1af80c63207f91
TimTim Wong <t2@live.hk>
2020-06-28 10:42:01 +0800

    Minify scripts

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
+set -euf -o pipefail
 mkdir -p dist && \
+/emsdk/node/*/bin/node /emsdk/upstream/emscripten/node_modules/.bin/google-closure-compiler \
+    --language_in ECMASCRIPT_NEXT \
+    --language_out ECMASCRIPT_2018 \
+    --js_output_file dist/rnnoise-runtime.js \
+    src/runtime.js && \
+/emsdk/node/*/bin/node /emsdk/upstream/emscripten/node_modules/.bin/google-closure-compiler \
+    --language_in ECMASCRIPT_NEXT \
+    --language_out ECMASCRIPT_2018 \
+    --js_output_file dist/rnnoise-processor.js \
+    src/processor.js && \
 emcc \
--s ENVIRONMENT=worker \
---no-entry \
--O3 -g0 \
--o dist/rnnoise-processor.wasm \
--Irnnoise/include \
-rnnoise/src/celt_lpc.c \
-rnnoise/src/denoise.c \
-rnnoise/src/kiss_fft.c \
-rnnoise/src/pitch.c \
-rnnoise/src/rnn.c \
-rnnoise/src/rnn_data.c \
-src/worklet.c
-cp src/processor.js dist/rnnoise-processor.js
-cp src/runtime.js dist/rnnoise-runtime.js
+    -s ENVIRONMENT=worker \
+    --no-entry \
+    -O3 -g0 \
+    -o dist/rnnoise-processor.wasm \
+    -Irnnoise/include \
+    rnnoise/src/celt_lpc.c \
+    rnnoise/src/denoise.c \
+    rnnoise/src/kiss_fft.c \
+    rnnoise/src/pitch.c \
+    rnnoise/src/rnn.c \
+    rnnoise/src/rnn_data.c \
+    src/worklet.c
--- dist/rnnoise-processor.js
+++ dist/rnnoise-processor.js
@@ -1,27 +1,2 @@
-registerProcessor("rnnoise", class extends AudioWorkletProcessor {
-    constructor(options) {
-        super({
-            ...options,
-            numberOfInputs: 1,
-            numberOfOutputs: 1,
-            outputChannelCount: [1]
-        });
-        Object.assign(this, new WebAssembly.Instance(options.processorOptions.module).exports);
-        this._heapFloat32 = new Float32Array(this.memory.buffer);
-        this.reset();
-        this._input = this.buffer(0);
-        this.port.onmessage = () => {
-            this.port.postMessage({ vadProb: this.getVadProb() });
-        };
-    }
-
-    process(input, output, parameters) {
-        const i = input[0][0], o = output[0][0];
-        this._heapFloat32.set(i, this._input / 4);
-        this._input = this.buffer(i.length);
-        const ptr4 = this.render(o.length) / 4;
-        if (ptr4)
-            o.set(this._heapFloat32.subarray(ptr4, ptr4 + o.length));
-        return true;
-    }
-});
+'use strict';registerProcessor("rnnoise",class extends AudioWorkletProcessor{constructor(a){super({...a,numberOfInputs:1,numberOfOutputs:1,outputChannelCount:[1]});Object.assign(this,(new WebAssembly.Instance(a.processorOptions.module)).exports);this._heapFloat32=new Float32Array(this.memory.buffer);this.reset();this._input=this.buffer(0);this.port.onmessage=()=>{this.port.postMessage({vadProb:this.getVadProb()})}}process(a,b,c){a=a[0][0];b=b[0][0];this._heapFloat32.set(a,this._input/4);this._input=
+this.buffer(a.length);(a=this.render(b.length)/4)&&b.set(this._heapFloat32.subarray(a,a+b.length));return!0}});
